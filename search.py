62d4943c03b0d9730b2d2f03e274efb23ecb532b
James <jbkuczma@buffalo.edu>
2016-01-25 14:52:58 -0500

    playing with command line arguments and performing a search via google

---
+++ search.py
@@ -0,0 +1,22 @@
+import argparse
+import webbrowser
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Search with your favorite browser and options")
+    parser.add_argument("browser", help="Browser of choice")
+    parser.add_argument("search", help="What you would like searched. *Quotes must be used*")
+    args = parser.parse_args()
+    search(args.search)
+
+
+def search(search):
+    try:
+        search = search.replace(' ', '+')
+        webbrowser.open_new("https://www.google.com/#q="+search)
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
